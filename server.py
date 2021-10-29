a0c3186aa0d773bb1ca771a01565246825802bd8
Christopher-Teague <cmteague77@gmail.com>
2021-10-29 15:04:48 -0700

    origin

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def table():
+    
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+    return render_template('index.html', names_list = users)
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
